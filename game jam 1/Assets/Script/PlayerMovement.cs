f10e4f597348f6453b681a2c4695c4ce8fd937c0
Strapple <bysfz767@gmail.com>
2025-05-31 18:31:29 +0700

    Animation

--- game jam 1/Assets/Script/PlayerMovement.cs
+++ game jam 1/Assets/Script/PlayerMovement.cs
@@ -2,32 +2,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpPower;
-    [SerializeField] private float teleportDistance;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpPower = 20f;
+    [SerializeField] private float teleportDistance = 3f;
     [SerializeField] private LayerMask obstacleLayer;
 
-    float horizontalInput;
-    bool isFacingRight = false;
-    bool isJumping = false;
+    private float horizontalInput;
+    private bool isFacingRight = false;
+    private bool isGrounded = false;
 
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-
         FlipSprite();
 
-        if (Input.GetButtonDown("Jump") && !isJumping)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
-            isJumping = true;
+            isGrounded = false;
+            animator.SetBool("isJumping", true);
         }
 
         if (Input.GetMouseButtonDown(2))
@@ -36,18 +38,20 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+        animator.SetFloat("yVelocity", rb.velocity.y);
     }
 
-    private void Teleport()
+    void Teleport()
     {
         float direction = isFacingRight ? 1f : -1f;
         Vector2 teleportDirection = new Vector2(direction, 0f);
 
         float actualDistance = CalculateSafeTeleportDistance(teleportDirection);
-        
+
         if (actualDistance > 0.1f)
         {
             Vector2 newPosition = new Vector2(
@@ -58,16 +62,16 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private float CalculateSafeTeleportDistance(Vector2 direction)
+    float CalculateSafeTeleportDistance(Vector2 direction)
     {
         float playerWidth = GetComponent<Collider2D>().bounds.size.x / 2;
         RaycastHit2D hit = Physics2D.BoxCast(
             origin: (Vector2)transform.position + (direction * playerWidth),
             size: new Vector2(0.1f, GetComponent<Collider2D>().bounds.size.y * 0.9f),
             angle: 0f,
-            direction : direction,
-            distance : teleportDistance,
-            layerMask : obstacleLayer
+            direction: direction,
+            distance: teleportDistance,
+            layerMask: obstacleLayer
         );
 
         if (hit.collider != null)
@@ -80,7 +84,7 @@ public class PlayerMovement : MonoBehaviour
 
     void FlipSprite()
     {
-        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        if ((isFacingRight && horizontalInput < 0f) || (!isFacingRight && horizontalInput > 0f))
         {
             isFacingRight = !isFacingRight;
             Vector3 ls = transform.localScale;
@@ -89,8 +93,12 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        isJumping = false;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            animator.SetBool("isJumping", false);
+        }
     }
 }
