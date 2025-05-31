92c0a23360b7cd03d2dfad747058f88af9cd32bf
Strapple <bysfz767@gmail.com>
2025-05-31 18:31:29 +0700

    Animation

--- game jam 1/Assets/Script/PlayerMovement.cs
+++ game jam 1/Assets/Script/PlayerMovement.cs
@@ -1,19 +1,29 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     float horizontalInput;
+<<<<<<< Updated upstream
     public float moveSpeed = 5f;
     bool isFacingRight = false;
     public float jumpPower = 5f;
     bool isJumping = false;
+=======
+    float moveSpeed = 5f;
+    bool isFacingRight = false;
+    public float jumpPower = 20f;
+    bool isGrounded = false;
+>>>>>>> Stashed changes
 
     Rigidbody2D rb;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,16 +33,26 @@ public class PlayerMovement : MonoBehaviour
 
         FlipSprite();
 
-        if (Input.GetButtonDown("Jump") && !isJumping)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+<<<<<<< Updated upstream
             isJumping = true;
+=======
+            isGrounded = false;
+            animator.SetBool("isJumping", !isGrounded);
+>>>>>>> Stashed changes
         }
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+<<<<<<< Updated upstream
+=======
+        animator.SetFloat("xVelocity", Math.Abs(rb.velocity.x));
+        animator.SetFloat("yVelocity", rb.velocity.y);
+>>>>>>> Stashed changes
     }
 
     void FlipSprite()
@@ -48,6 +68,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        isJumping = false;
+        if (collision.gameObject.CompareTag("Ground")) // Ensure your ground objects have the "Ground" tag
+        {
+            isGrounded = true;
+            animator.SetBool("isJumping", !isGrounded);
+        }
     }
 }
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
